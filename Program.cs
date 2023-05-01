c78d73edde7a1269af77bcc18351cd3410b2f669
Hrodvitnir-Fenrir <44929201+Hrodvitnir-Fenrir@users.noreply.github.com>
2023-05-01 21:35:40 +0200

    what i'm doing

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace WallpaperWithArtStation
+{
+    class Program
+    {
+        static async Task<string> FetchRandomArtworkUrlAsync()
+        {
+            using HttpClient client = new HttpClient();
+            string apiUrl = "https://www.artstation.com/random_project.json";
+            string response = await client.GetStringAsync(apiUrl);
+
+            JsonDocument jsonDoc = JsonDocument.Parse(response);
+            string imageUrl = jsonDoc.RootElement.GetProperty("assets")[0].GetProperty("image_url").GetString();
+
+            if (imageUrl == null) {
+                imageUrl = "https://cdnb.artstation.com/p/assets/images/images/053/557/633/4k/kyle-enochs-week-09-assignment-01-image-01.jpg?1662489961";
+            }
+            return imageUrl;
+        }
+
+        static async Task<string> DownloadImageAsync(string imageUrl)
+        {
+            string tempFilePath = Path.GetTempFileName() + ".jpg";
+
+            using HttpClient client = new HttpClient();
+            using HttpResponseMessage response = await client.GetAsync(imageUrl);
+            using FileStream fileStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write, FileShare.None);
+            await response.Content.CopyToAsync(fileStream);
+
+            return tempFilePath;
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        static void SetWallpaper(string imagePath)
+        {
+            const int SPI_SETDESKWALLPAPER = 0x0014;
+            const int SPIF_UPDATEINIFILE = 0x01;
+            const int SPIF_SENDCHANGE = 0x02;
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imagePath, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
+
+        static async Task Main(string[] args)
+        {
+            string imageUrl = await FetchRandomArtworkUrlAsync();
+            string imagePath = await DownloadImageAsync(imageUrl);
+            SetWallpaper(imagePath);
+            File.Delete(imagePath);
+        }
+    }
+}
